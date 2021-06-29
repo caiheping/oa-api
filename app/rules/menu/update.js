df48c23f27292c6d61d59992998909e0750ced3e
chp <1670341607@qq.com>
2021-06-29 18:18:25 +0800

    完善代码

--- app/rules/menu/update.js
+++ app/rules/menu/update.js
@@ -11,7 +11,7 @@ const rule = {
     { required: true, message: '菜单名称不能为空' },
   ],
   orderNum: [
-    { required: true, message: '菜单名称不能为空' },
+    { required: true, message: '显示顺序不能为空' },
   ],
 };
 
