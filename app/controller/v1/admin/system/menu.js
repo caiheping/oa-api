c4abeb32eeee22084df80b5e62b92c6e513f1de7
caiheping <caihp@yunjisoft.com>
2021-12-07 17:54:56 +0800

    优化菜单管理时选择外链时的bug

--- app/controller/v1/admin/system/menu.js
+++ app/controller/v1/admin/system/menu.js
@@ -43,6 +43,68 @@ class Controller extends BaseController {
     const result = await service[this.app.config.public].admin[this.modleName][this.serviceName].findByUser();
     ctx.returnBody(result, 100010);
   }
+
+  async create() {
+    const { ctx, service } = this;
+    const validateResult = await ctx.checkValidate(ctx.request.body, this.serviceName + '.create');
+    if (!validateResult) return;
+    const query = ctx.request.body;
+    query.createdAt = new Date();
+    query.createdBy = ctx.state.user.userName;
+    if (query.path) {
+      const componentName = query.path.replace(/^\//, '');
+      if (query.isFrame === '1') {
+        // 校验URL地址合法性
+        const reg =
+        /^(https?|ftp|file):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]$/i;
+        if (reg.test(query.path)) {
+          query.name = query.path;
+        } else {
+          throw Error('外链的URL（路由）地址不合法！');
+        }
+      } else {
+        query.name = componentName.charAt(0).toUpperCase() + componentName.slice(1);
+      }
+    }
+    const result = await service[this.app.config.public].admin[this.modleName][this.serviceName].create(query);
+    if (result) {
+      ctx.returnBody(null, 100020);
+    } else {
+      ctx.returnBody(null, 100021, 500);
+    }
+  }
+
+  async update() {
+    const { ctx, service } = this;
+    const validateResult = await ctx.checkValidate(ctx.request.body, this.serviceName + '.update');
+    if (!validateResult) return;
+    const query = ctx.request.body;
+    query.updatedAt = new Date();
+    query.updatedBy = ctx.state.user.userName;
+    const id = this.ctx.helper.parseInt(ctx.params.id);
+    if (query.path) {
+      const componentName = query.path.replace(/^\//, '');
+      if (query.isFrame === '1') {
+        const reg =
+        /^(https?|ftp|file):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]$/i;
+        if (reg.test(query.path)) {
+          query.name = query.path;
+        } else {
+          throw Error('外链的URL（路由）地址不合法！');
+        }
+      } else {
+        query.name = componentName.charAt(0).toUpperCase() + componentName.slice(1);
+      }
+    }
+    const result = await service[this.app.config.public].admin[this.modleName][this.serviceName].update(query, {
+      id,
+    });
+    if (result) {
+      ctx.returnBody(null, 100030);
+    } else {
+      ctx.returnBody(null, 100031, 500);
+    }
+  }
 }
 
 module.exports = Controller;
